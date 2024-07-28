5fc6f3077102ede6152ac1434d6ac7909c197f0a
Yashdarji77 <yashdarji2403@gmail.com>
2024-07-28 12:58:53 +0530

    now its resopnisve also

--- public/javascripts/blob.js
+++ public/javascripts/blob.js
@@ -1,3 +1,16 @@
+let icon = document.querySelector(".icon");
+let ul = document.querySelector("ul");
+
+icon.addEventListener("click", () => {
+    ul.classList.toggle("showData");
+
+    if (ul.className == "showData") {
+        document.getElementById("bar").className = "fa-solid fa-xmark";
+    } else {
+        document.getElementById("bar").className = "fa-solid fa-bars";
+    }
+})
+
 document.addEventListener('DOMContentLoaded', () => {
     const likeButtons = document.querySelectorAll('.comment-buttons button:first-of-type');
     const dislikeButtons = document.querySelectorAll('.comment-buttons button:last-of-type');
--- public/javascripts/review.js
+++ public/javascripts/review.js
@@ -1,3 +1,16 @@
+let icon = document.querySelector(".icon");
+let ul = document.querySelector("ul");
+
+icon.addEventListener("click", () => {
+    ul.classList.toggle("showData");
+
+    if (ul.className == "showData") {
+        document.getElementById("bar").className = "fa-solid fa-xmark";
+    } else {
+        document.getElementById("bar").className = "fa-solid fa-bars";
+    }
+})
+
 document.addEventListener('DOMContentLoaded', () => {
     const reviews = document.querySelectorAll('.rCard');
     const filterInput = document.getElementById('filter-input');
